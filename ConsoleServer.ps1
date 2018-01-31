763f66bd2bed84bf75288734f97150b1aaaa830a
Ryan Boudreaux <boudreauxryan@gmail.com>
2018-01-31 23:14:49 +0000

    Update and rename coms.ps1 to ConsoleServer.ps1

--- ConsoleServer.ps1
+++ ConsoleServer.ps1
@@ -1,9 +1,9 @@
 ﻿param([int]$port=8080, [string]$message="OK")
-$listener = New-Object System.Net.HttpListener
-$listener.Prefixes.Add("http://localhost:$port/")
-$listener.Start()
+$interface = New-Object System.Net.HttpListener
+$interface.Prefixes.Add("http://localhost:$port/")
+$interface.Start()
 while ($listener.IsListening)  {
-    $context = $listener.GetContext()  #Blocks
+    $context = $interface.GetContext()  #Blocks
     $buffer = [System.Text.Encoding]::UTF8.GetBytes($message )
     $context.response.OutputStream.Write($buffer, 0, $buffer.Length)   
     $context.response.Close();
