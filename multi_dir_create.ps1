f8c8f50f3a8e8a711cd0f7f4cbb8aa2e86b9dee1
Ryan Boudreaux <boudreauxryan@gmail.com>
2018-05-25 14:37:25 -0500

    Update and rename One-liners to multi_dir_create.ps1

---
+++ multi_dir_create.ps1
@@ -0,0 +1,8 @@
+# command to create multiple directories at once
+echo dir1 dir2 dir3 | foreach $_{mkdir $_}
+
+# or
+dir1 dir2 dir3 | foreach $_{ New-Item -ItemType directory -Name $_ }
+
+## For numerical directories, ex. ch1, ch2 ...
+0..9 | foreach $_{ New-Item -ItemTyp
