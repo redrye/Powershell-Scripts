593052fb20c3931ced19f5428a79a9fe718c16d0
Ryan Boudreaux <boudreauxryan@gmail.com>
2018-05-25 14:37:52 -0500

    Update multi_dir_create.ps1

--- multi_dir_create.ps1
+++ multi_dir_create.ps1
@@ -5,4 +5,4 @@ echo dir1 dir2 dir3 | foreach $_{mkdir $_}
 dir1 dir2 dir3 | foreach $_{ New-Item -ItemType directory -Name $_ }
 
 ## For numerical directories, ex. ch1, ch2 ...
-0..9 | foreach $_{ New-Item -ItemTyp
+0..9 | foreach $_{ New-Item -ItemType directory -Name $("ch" + $_) }
